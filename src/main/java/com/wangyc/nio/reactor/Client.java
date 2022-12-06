8ed885a43fed206cf60e3529580dd9d0a91ba806
cczyWyc <274230928@qq.com>
2022-12-06 16:24:04 +0800

    nio reactor: add client

---
+++ src/main/java/com/wangyc/nio/reactor/Client.java
@@ -0,0 +1,12 @@
+package com.wangyc.nio.reactor;
+
+/**
+ * nio client, send request to server
+ *
+ * @author cczyWyc
+ */
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/wangyc/nio/reactor/Handler.java
@@ -0,0 +1,9 @@
+package com.wangyc.nio.reactor;
+
+/**
+ * handle the business
+ *
+ * @author cczyWyc
+ */
+public class Handler {
+}
